b0b6d24cbf49cca35fc073097480491449e65bf6
ReginaldCosensIII <ReginaldCosensIII@gmail.com>
2025-04-08 14:29:37 -0400

    Updated to use DATABASE_URL from environment V1.1 TYPO correction

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-DATABASE_URL = os.environ.get("DATABASE_URL") or "postgresql://admin:plAA1t13QVCmkRQFr2bGoAbpJNLeKUrQ@dpg-cvqllt49c44c73bgi16g-a.virginia-postgres.render.com/bookstore_db_40sirl"
+DATABASE_URL = os.environ.get("DATABASE_URL") or "postgresql://admin:plAA1t13QVCmkRQFr2bGoAbpJNLeKUrQ@dpg-cvqllt49c44c73bgi16g-a.virginia-postgres.render.com/bookstore_db_40si"
 
 # Database connection
 def get_db_connection():
