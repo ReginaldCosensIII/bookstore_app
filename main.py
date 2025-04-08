577cae3a38e0fb8a82fa6be351cc3e8a96a7ff3d
ReginaldCosensIII <ReginaldCosensIII@gmail.com>
2025-04-08 02:39:57 -0400

    Updated local database URL

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-DATABASE_URL = "postgresql://postgres:Bigpimp@localhost:5433/bookstore_db"
+DATABASE_URL = "postgres://postgres:Bigpimp@localhost:5433/bookstore_db"
 
 # Database connection
 def get_db_connection():
