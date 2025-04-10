8a5ce325077214d295bea434fdfa7da7d0548ee9
ReginaldCosensIII <ReginaldCosensIII@gmail.com>
2025-04-09 21:09:51 -0400

    Update __init__.py
    added the template folder template_folder="templates"

--- bookstore_app/app/__init__.py
+++ bookstore_app/app/__init__.py
@@ -4,6 +4,6 @@ from .routes import bp as main_bp
 def create_app():    
     # Create a Flask application instance.
     # Register the main blueprint with the application.
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates")
     app.register_blueprint(main_bp)
     return app
